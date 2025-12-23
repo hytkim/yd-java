23c3acb1f9b33e6ed18f90005da30ee927e08ba7
hytkim <145666445+hytkim@users.noreply.github.com>
2025-12-23 18:40:05 +0900

    김현태/Page-182~183/25-12-23
    Q1~Q6

--- chapter04/src/com/yedam/java/ch0402/Page182To183.java
+++ chapter04/src/com/yedam/java/ch0402/Page182To183.java
@@ -1,6 +1,6 @@
 package com.yedam.java.ch0402;
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class Page182To183 {
 
@@ -15,14 +15,50 @@ public class Page182To183 {
 		}
 		System.out.println("sum = " + sum);
 
-		Scanner sc = new Scanner(System.in);
+		//Scanner sc = new Scanner(System.in);
 		// Q3 : Math.random
 		while (true) {
 			int d1 = (int)(Math.random() * 6) + 1;
 			int d2 = (int)(Math.random() * 6) + 1;
 			
-			System.out.printf("주사위 2개 결과 (%d, %d)", d1, d2);
+			System.out.printf("주사위 2개 결과 (%d, %d)\n", d1, d2);
+			if (d1 + d2 == 5) {
+				break;
+			}
 		}
+		
+		// Q4 : 4x + 5y = 60 의 모든 해
+		for (int i = 1; i < 10; i++) {
+			
+			for (int j = 1; j < 10; j++) {
+				if (4*i + 5*j == 60) 
+					System.out.println("x = " + i + ", y = "+ j);
+			}
+		}
+		
+		// Q5 : ㄴ 삼각형
+		for (int i = 0; i < 5; i++) {
+			String str = "";
+			for (int j = 5-(5-i); j > 0; j--) {				
+				str += "*";
+			}
+			System.out.println(str);
+		}
+		System.out.println("--------------------------------------");
+		// Q6 : 역ㄴ 삼각형
+		for (int i = 0; i < 4; i++) {
+			String str = "";
+			for (int j = 0; j < 5; j++) {
+				if (j+i < 4) {
+					str += " ";					
+				}
+				else {
+					str += "*";
+				}
+			}
+			System.out.println(str);
+		}
+		
 	}
 
 }
